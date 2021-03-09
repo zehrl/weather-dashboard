7af539e8b82e123f62dfdceedf16b6c16a3d8d87
Logan Zehr <zehrl315@outlook.com>
2021-03-08 16:35:11 -0800

    getCoordinates async function works.

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,66 @@
-const cityName = "Fort Wayne"
-const apiKey = "e452aa651bd80481f1b2311b6f81f11d"
+$(function () {
+    const apiKey = "e452aa651bd80481f1b2311b6f81f11d"
 
+    // Handle initial loading of data
 
-// Weather
+    // Handle form submit
 
-const findWeather = () => {
+    // Render search history
 
-    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
-    
-    $.ajax({
-        url: weatherUrl,
-        type: "get"
-    }).done(function (response) {
-        // Current Temperature
-        console.log(`Current Temp = ${response.main.temp} F`)
-        
-        // Current Humidity
-        console.log(`Current Humidity = ${response.main.humidity}%`)
-        
-        // Current Wind Speed
-        console.log(`Current Wind Speed = ${response.wind.speed} mph`)
-        
-        // Coordinates
-        console.log(`Lat: ${response.coord.lat}, Long: ${response.coord.lon}`)
-        findUvIndex(response.coord.lat, response.coord.lon)
-        
-        
-        // console.log(response)
-    });
-    
-}
+    // Render today's forecast
 
-const findUvIndex = (lat, lon) => {
+    // Render 5-day forecast
 
-    // UV Index
-    const uvIndexUrl = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    // get today's forecast
 
-    $.ajax({
-        url: uvIndexUrl,
-        type: "get"
-    }).done(function (response) {
-        // Current UV Index
-        console.log(`Current UV Index = ${response.value}`)
+    const getCoordinates = async (cityName) => {
 
-        // console.log(response)
-    });
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`
+        let coordinates = {lat: "N/A", lon: "N/A"};
 
-}
+        await $.ajax({
+            url: url,
+            type: "get"
+        }).done(function (response) {
+            console.log(`Coordinate Response: `, response)
+            coordinates = {
+                lat: response[0].lat,
+                lon: response[0].lon
+            }
+        });
+
+        return coordinates;
+    }
+
+    const getData = async() => {
+        let coordinates = await getCoordinates("Fort Wayne");
+        // let weather = await getWeather();
+        console.log("Await coordinates: ", coordinates);
+    }
+
+    getData();
+
+    // const getWeather = async (lat, lon) => {
+
+    //     // UV Index
+    //     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+
+    //     console.log("getting weather...")
+
+    //     // await $.ajax({
+    //     //     url: url,
+    //     //     type: "get"
+    //     // }).done(function (response) {
+    //     //     // Current UV Index
+
+    //     //     console.log('Weather Response: ', response)
+    //     //     console.log(`Current UV Index = ${response.value}`)
+
+    //     //     // console.log(response)
+    //     // });
+
+    //     setTimeout(()=>{return "test"}, 5000)
+
+    // }
+
+})
