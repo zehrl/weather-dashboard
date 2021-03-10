f7a63d5cbd1113092eec5baea3169df510409df4
Logan Zehr <zehrl315@outlook.com>
2021-03-10 15:23:22 -0800

    Added alt text for each 5-day forecast card

--- scripts/script.js
+++ scripts/script.js
@@ -107,7 +107,7 @@ $(function () {
 
             // Icon
             const iconCode = weatherAr[dayIndex-1].weather[0].icon;
-            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`
+            const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@4x.png`
 
             // Temperature - reound to nearest degree
             const temp = Math.round(parseInt(weatherAr[dayIndex - 1].temp.day)).toString();
